f377a112da07e38434c983998e9f3929a4e9063f
mark <sijver@gmail.com>
2013-04-04 17:32:50 +0200

    Abbreviation has been deleted due to disusing

--- src/main/core/Neighbourhood.java
+++ src/main/core/Neighbourhood.java
@@ -4,17 +4,7 @@ package main.core;
  * Created with IntelliJ IDEA.
  */
 public enum Neighbourhood {
-    TRANSPOSE("T"),
-    INSERT("I"),
-    EXCHANGE("E");
-
-    private Neighbourhood(String abbreviation) {
-        this.abbreviation = abbreviation;
-    }
-
-    public String getAbbreviation() {
-        return abbreviation;
-    }
-
-    private String abbreviation;
+    TRANSPOSE,
+    INSERT,
+    EXCHANGE;
 }
--- src/main/core/PivotingRule.java
+++ src/main/core/PivotingRule.java
@@ -4,16 +4,6 @@ package main.core;
  * Created with IntelliJ IDEA.
  */
 public enum PivotingRule {
-    FIRST_IMPROVEMENT("First"),
-    BEST_IMPROVEMENT("Best");
-
-    private PivotingRule(String abbreviation) {
-        this.abbreviation = abbreviation;
-    }
-
-    public String getAbbreviation() {
-        return abbreviation;
-    }
-
-    String abbreviation;
+    FIRST_IMPROVEMENT,
+    BEST_IMPROVEMENT;
 }
